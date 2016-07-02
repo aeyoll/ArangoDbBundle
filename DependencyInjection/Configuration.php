a2ccffcf4d325bfb2e43f3b0e90f59f298822501
Jean-Philippe Bidegain <jp@bidega.in>
2016-07-02 17:46:28 +0200

    Bundle configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ namespace Aeyoll\Bundle\ArangoDbBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use triagens\ArangoDb\UpdatePolicy;
 
 /**
  * This is the class that validates and merges configuration from your app/config files.
@@ -20,9 +21,34 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('aeyoll_arango_db');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->scalarNode('default_connection')->defaultNull()->end()
+                ->arrayNode('connection')
+                    ->useAttributeAsKey('name')
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode('database')->end()
+                            ->scalarNode('endpoint')->end()
+                            ->scalarNode('auth_user')->end()
+                            ->scalarNode('auth_passwd')->end()
+                            ->scalarNode('auth_type')->defaultValue('Basic')->end()
+                        ->end()
+                    ->end()
+                ->end()
+
+                ->arrayNode('options')
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode('connection')->defaultValue('Keep-Alive')->end()
+                            ->integerNode('timeout')->defaultValue(30)->end()
+                            ->booleanNode('reconnect')->defaultTrue()->end()
+                            ->booleanNode('create')->defaultTrue()->end()
+                            ->scalarNode('update_policy')->defaultValue(UpdatePolicy::LAST)->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
 
         return $treeBuilder;
     }
